d0f31a35852fa19803babd5167866847a357abc0
Ivan Johnson <git@IvanJohnson.net>
2019-03-18 10:18:09 -0500

    Make world's attributes public

--- world.py
+++ world.py
@@ -5,8 +5,8 @@ import equipment
 class world:
     def __init__(self, bandwidth: float, cEquipment: int, N0: float):
         assert(cEquipment>0)
-        self._bandwidth = bandwidth
-        self._N0 = N0
-        self._cEquipment = cEquipment
+        self.bandwidth = bandwidth
+        self.N0 = N0
+        self.cEquipment = cEquipment
 
-        self._equipment = [config.newEquipment(self) for i in range(cEquipment)]
+        self.equipment = [config.newEquipment(self) for i in range(cEquipment)]
