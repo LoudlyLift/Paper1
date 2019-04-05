6358bf79d70f90f6e84cf6fbb590a3d6d16a42df
Ivan Johnson <git@IvanJohnson.net>
2019-04-05 11:04:25 -0500

    Make equipment a self-contained module

--- equipment.py
+++ equipment.py
@@ -4,23 +4,38 @@ import random
 import task
 import world
 
+"""
+
+cbInput -- the byte count of the input cCycle
+
+size -- the count of the number of CPU cycles it will take to compute this task
+
+sDelayMax -- maximum tolerable delay of the task, in seconds
+
+timeenergy_ratio -- the linear tradeoff to make between optimizing energy
+consumption and optimizing the time until completion. One means complete the
+task as soon as possible without regard to energy costs. Zero means that
+equipment's task is to be completed in the most energy efficient manner possible
+(as long as it is still completed within sDelayMax seconds).
+"""
 class equipment:
-    def __init__(self, world_: world.world, power: float, power_waiting: float,
-                 gain, task_, frequency: float, energyPerCycle: float,
-                 timeenergy_ratio: float):
-        self.world_ = world_
+    def __init__(self, power: float, power_waiting: float, gain, frequency:
+                 float, energyPerCycle: float, timeenergy_ratio: float, cbInput:
+                 int, cCycle: int, sDelayMax: float):
         self.power = power
         self.power_waiting = power_waiting
         self.gain = gain
-        self.task_ = task_
         self.frequency = frequency
         self.energyPerCycle = energyPerCycle
         self.timeenergy_ratio = timeenergy_ratio
+        self.cbInput = cbInput
+        self.cCycle = cCycle
+        self.sDelayMax = sDelayMax
 
-    def _compute_upload_rate(self, effective_bandwidth):
+    def _compute_upload_rate(self, effective_bandwidth, N0):
         """Computes the unscaled upload rate using the formula given in eq. (1)"""
         num=self._power*self._gain
-        dem=effective_bandwidth*self.world_._N0
+        dem=effective_bandwidth*N0
         quotient=num/dem
 
         #TODO: log_e, I presume?
@@ -28,26 +43,25 @@ class equipment:
 
     def cost_local(self):
         #eq. (2)
-        time   = self.task_.cCycle / self.frequency
+        time   = self.cCycle / self.frequency
         #eq. (3)
-        energy = self.task_.cCycle * self.energyPerCycle
+        energy = self.cCycle * self.energyPerCycle
 
         #eq. after (3) but before (4)
         return time*self.timeenergy_ratio \
             + energy*(1-self.timeenergy_ratio)
 
-    def cost_offload(self, cOffloaders: int, allocatedClockSpeed: float):
-        effective_bandwidth = self.world_.bandwidth / cOffloaders
-
+    def cost_offload(self, effective_bandwidth: float, cOffloaders: int,
+                     allocatedClockSpeed: float):
         # eq. (4)
-        time_offload = self.task_.cbInput / \
+        time_offload = self.cbInput / \
             self._compute_upload_rate(effective_bandwidth)
 
         # eq. (5)
         energy_offload = self.power * time_offload
 
         # eq. (6)
-        time_processing = self.task_ / allocatedClockSpeed
+        time_processing = self.cCycle / allocatedClockSpeed
 
         # eq. (7)
         energy_waiting = self.power_waiting * time_processing
