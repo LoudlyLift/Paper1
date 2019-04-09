dda182b2b2729b687e2a15ee47a94b404399acf3
Ivan Johnson <git@IvanJohnson.net>
2019-04-08 19:01:56 -0500

    Merge separate runners into one with argparse

--- runner.py
+++ runner.py
@@ -1,15 +1,29 @@
 import time
+import sys
+import argparse
 
 import algSmart_world
+import alg1_world
 import config
 import equipment
 import qlearning
 import qtable
 import statistics
 
+parser = argparse.ArgumentParser(description='Run edge computing simulations')
+parser.add_argument('algorithm', choices=['one', 'smart'])
+parser.add_argument('--train-episodes', type=int, default=10000, help="How many episodes to perform during training")
+parser.add_argument('--eval-episodes', type=int, default=1000, help="How many episodes to perform during evaluation")
+args = parser.parse_args()
+
 s = config.newSimulation()
 
-w = algSmart_world.algSmart_world(s, config.equipmentToState, config.equipmentStateMetadata)
+if args.algorithm == 'smart':
+    w = algSmart_world.algSmart_world(s, config.equipmentToState, config.equipmentStateMetadata)
+elif args.algorithm == 'one':
+    w = alg1_world.alg1_world(s)
+else:
+    assert(False)
 
 ql = qlearning.qlearning(env=w,
                          compute_randact=lambda episodeNum: 0.1,
@@ -19,12 +33,11 @@ ql = qlearning.qlearning(env=w,
 
 t1 = time.time()
 
-#alg 1: 3**6 states, 4 actions => 2916 states
-ql.train(1000000) #approx. 1k / 25min
+ql.train(args.train_episodes)
 
 t2 = time.time()
 
-results = ql.evaluate(1000)
+results = ql.evaluate(args.eval_episodes)
 
 avgmin    = statistics.median(result["min"]    for result in results)
 avgmax    = statistics.median(result["max"]    for result in results)
@@ -35,6 +48,6 @@ print("TRAINING:")
 print(f"Training duration: {(t2-t1):.2f}")
 print(f"Number of updates: {ql.getTrainUpdateCount()}")
 print(f"Numver of entries in Q-Table: {ql.player._table.size}")
-
+print("")
 print("EVALUATION:")
-print(f"min: {avgmin:.2f}; max: {avgmax:.2f}; local: {avglocal:.2f}; actual: {avgactual:.2f}")
+print(f"min: {avgmin:.2f}\nmax: {avgmax:.2f}\nlocal: {avglocal:.2f}\nactual: {avgactual:.2f}")
